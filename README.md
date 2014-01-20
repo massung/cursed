5980ac31bd5423204514ddb31c801030649dbddd
Jeffrey Massung <massung@gmail.com>
2014-01-20 00:01:16 -0600

    Initial first pass. Added README.

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 The `cursed` package adds a subclass of the [output-pane](http://www.lispworks.com/documentation/lw61/CAPRM/html/capiref-275.htm#marker-4173290) element in [LispWorks'](http://www.lispworks.com) [CAPI](http://www.lispworks.com/documentation/lw61/CAPUG-M/html/capiuser-m.htm). It's designed to allow for simple, console-style display of text, but within a graphical UI.
 
+## Quickstart
+
+You can immediately start using the `cursed-pane` by just containing one.
+
+	CL-USER > (setf c (capi:contain (make-instance 'cursed-pane)))
+	#<CURSED-PANE  21C83D3B>
+	
+![Initial pane with cursor visible](./screenshots/cursed-pane-01.png)
+	
+Once the pane is up, you can use one of the many available print functions to render text on it.
+
+	CL-USER > (princ-to-pane "Hello, world!" c :foreground :red)
+	NIL
+
+![With obligatory example](./screenshots/cursed-pane-02.png)
+
+You can use the `cursed-pane-cursor-x` and `cursed-pane-cursor-y` accessor methods to `setf` the cursor position. The cursor can also be toggled with the `cursed-pane-cursor-visible-p` accessor.
+
+
--- cursed.lisp
+++ cursed.lisp
@@ -20,7 +20,104 @@
 (defpackage :cursed
   (:use :cl :capi)
   (:export
+   #:cursed-pane
+
+   ;; accessors
+   #:cursed-pane-chars
+   #:cursed-pane-chars-wide
+   #:cursed-pane-chars-high
+   #:cursed-pane-cursor-x
+   #:cursed-pane-cursor-y
+   #:cursed-pane-cursor-visible-p
+
+   ;; methods
+   #:princ-to-pane
    ))
 
 (in-package :cursed)
 
+(defclass cursed-pane (output-pane)
+  ((chars)
+
+   ;; the size of the pane
+   (chars-wide     :initarg :chars-wide     :initform 80 :reader cursed-pane-chars-wide)
+   (chars-high     :initarg :chars-high     :initform 24 :reader cursed-pane-chars-high)
+
+   ;; true if the cursor should be drawn
+   (cursor-visible :initarg :cursor-visible :initform t  :accessor cursed-pane-cursor-visible-p)
+
+   ;; xy location of the cursor
+   (cursor-x       :initarg :cursor-x       :initform 0  :accessor cursed-pane-cursor-x)
+   (cursor-y       :initarg :cursor-y       :initform 0  :accessor cursed-pane-cursor-y))
+  (:default-initargs
+   :background :black
+   :foreground :gray90
+   :font (gp:make-font-description :family "Courier" :size 12.0)
+   :create-callback 'create-cursed-pane
+   :display-callback 'display-cursed-pane))
+
+(defstruct glyph (char " ") bg fg)
+
+(defmethod create-cursed-pane ((pane cursed-pane))
+  "Set the size of the pane and clear the output."
+  (with-slots (chars chars-wide chars-high)
+      pane
+    (set-hint-table pane (list :visible-min-width (list 'character chars-wide)
+                               :visible-max-width (list 'character chars-wide)
+                               :visible-min-height (list 'character chars-high)
+                               :visible-max-height (list 'character chars-high)))
+
+    ;; allocate an array of strings for all the characters (column major)
+    (setf chars (make-array (list chars-wide chars-high) :element-type 'glyph))
+
+    ;; fill in the characters with default glyphs
+    (dotimes (x chars-wide)
+      (dotimes (y chars-high)
+        (setf (aref chars x y) (make-glyph))))))
+
+(defmethod display-cursed-pane ((pane cursed-pane) x y w h)
+  "Redraw characters in the pane."
+  (with-slots (chars cursor-x cursor-y cursor-visible)
+      pane
+    (let ((cw (gp:get-font-width pane))
+          (ch (gp:get-font-height pane))
+          (fd (gp:get-font-descent pane)))
+
+      ;; erase the area that is to be redrawn
+      (gp:draw-rectangle pane x y w h :filled t :foreground (simple-pane-background pane))
+
+      ;; render the block cursor first
+      (when cursor-visible
+        (gp:draw-rectangle pane (* cursor-x cw) (* cursor-y ch) (1+ cw) (1+ ch) :filled t))
+
+      ;; render all the characters in the area that needs redrawn
+      (flet ((blit-glyph (x y &aux (glyph (aref chars x y)))
+               (with-slots (char bg fg)
+                   glyph
+                 (when bg
+                   (gp:draw-rectangle pane (* x cw) (* y ch) (1+ cw) (1+ ch) :filled t :foreground bg))
+                 (gp:draw-string pane char (* x cw) (- (+ (* y ch) ch) fd) :foreground fg))))
+        (loop :for i :from (truncate x cw) :below (truncate (+ x w) cw) :do
+              (loop :for j :from (truncate y ch) :below (truncate (+ y h) ch) :do
+                    (blit-glyph i j)))))))
+
+(defmethod princ-to-pane (value (pane cursed-pane) &key background foreground)
+  "Output a value at the current cursor position."
+  (with-slots (chars cursor-x cursor-y chars-wide chars-high)
+      pane
+    (loop :for c :across (princ-to-string value)
+          :do (let ((glyph (aref chars cursor-x cursor-y)))
+                (setf (glyph-char glyph) (string c))
+
+                ;; optional color settings
+                (setf (glyph-bg glyph) background)
+                (setf (glyph-fg glyph) foreground)
+
+                ;; advance the cursor
+                (when (>= (incf cursor-x) chars-wide)
+                  (if (>= (incf cursor-y) chars-high)
+                      (return nil)
+                    (setf cursor-x 0)))))
+    
+    ;; TODO: limit the redraw to only what needs to be done
+    (gp:invalidate-rectangle pane)))
